b62bfebeb6ab64cf782f0d37652b4b7ba547120b
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-14 11:07:55 -0500

    places model

--- models/places.js
+++ models/places.js
@@ -1,3 +1,27 @@
 const mongoose = require('mongoose')
 
-const placeSchema = new mongoose.Schema()
+const placeSchema = new mongoose.Schema({
+  place: {
+    type: String,
+    required: true
+  },
+  pic: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  city: { 
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
